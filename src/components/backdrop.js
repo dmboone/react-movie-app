f0ce0ee8cab37dbe355c41c86eeab414d5e0259b
dmboone <destaj.db@gmail.com>
2023-10-25 15:41:42 -0400

    homepage with animations complete

--- src/components/backdrop.js
+++ src/components/backdrop.js
@@ -5,27 +5,38 @@ import "./backdrop.css";
 const backdropMovies = getBackdropMovies();
 
 const Backdrop = () => {
-    const [backdropMovieIndex, setBackdropMovieIndex] = useState(0);
+    const [currentBackdropMovieIndex, setCurrentBackdropMovieIndex] = useState(0);
+    const [oldBackdropMovieIndex, setOldBackdropMovieIndex] = useState(backdropMovies.length - 1);
 
     useEffect(() => {
-        // Change backdrop image every 8 seconds
+        // Change backdrop image every 15 seconds
         const interval = setInterval(
-            () => setBackdropMovieIndex((backdropMovieIndex + 1) % backdropMovies.length),
-            8000
+            () => {
+                setCurrentBackdropMovieIndex((currentBackdropMovieIndex + 1) % backdropMovies.length);
+                setOldBackdropMovieIndex((oldBackdropMovieIndex + 1) % backdropMovies.length);
+            }, 15000
         );
 
         // Clearing the interval
         return () => clearInterval(interval);
         
-    }, [backdropMovieIndex]);
+    }, [currentBackdropMovieIndex, oldBackdropMovieIndex]);
 
     return(
         <>
-            <h2 className="backdrop-text">{backdropMovies[backdropMovieIndex].original_title}</h2>
-            <div className="overlay"></div>
             <div className="backdrop-wrapper">
-                <img className="backdrop-image" src={`https://image.tmdb.org/t/p/original/${backdropMovies[backdropMovieIndex].backdrop_path}`} alt={backdropMovies[backdropMovieIndex].original_title} />
+                <h2 className="backdrop-text reveal-text">{backdropMovies[currentBackdropMovieIndex].original_title}</h2>
             </div>
+            {/* Current Backdrop Image */}
+            <div className="backdrop-wrapper">
+                <img className="current-backdrop-img fade-in-image" src={`https://image.tmdb.org/t/p/original/${backdropMovies[currentBackdropMovieIndex].backdrop_path}`} alt={backdropMovies[currentBackdropMovieIndex].original_title} />
+            </div>
+            {/* Old Backdrop Image */}
+            <div className="backdrop-wrapper">
+                <img className="old-backdrop-img fade-out-image" src={`https://image.tmdb.org/t/p/original/${backdropMovies[oldBackdropMovieIndex].backdrop_path}`} alt="" />
+            </div>
+            <div className="overlay"></div>
+
         </>
     );
 }
