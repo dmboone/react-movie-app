ca31621e269b24c41126f149b645b6072731353e
dmboone <destaj.db@gmail.com>
2023-10-27 03:07:09 -0400

    text animations

--- src/components/backdrop.js
+++ src/components/backdrop.js
@@ -23,7 +23,8 @@ const Backdrop = () => {
     return(
         <>
             <div style={{overflow: "hidden", position: "relative", width: "100vw", height: "75%"}}>
-                <h2 className="backdrop-text reveal-text">{backdropMovies[currentBackdropMovieIndex % backdropMovies.length].original_title}</h2>
+                <h2 className="backdrop-text reveal-text" key={`${currentBackdropMovieIndex}_revealText` }>{backdropMovies[currentBackdropMovieIndex % backdropMovies.length].original_title}</h2>
+                {/* <h2 className="backdrop-text hide-text" key={`${currentBackdropMovieIndex}_hideText` }>{backdropMovies[currentBackdropMovieIndex % backdropMovies.length].original_title}</h2> */}
                 {/* Current Backdrop Image */}
                 <img className="backdrop-wrapper current-backdrop-img fade-in-image" key={currentBackdropMovieIndex} src={`https://image.tmdb.org/t/p/original/${backdropMovies[currentBackdropMovieIndex % backdropMovies.length].backdrop_path}`} alt={backdropMovies[currentBackdropMovieIndex % backdropMovies.length].original_title} />
                 {/* Old Backdrop Image */}
